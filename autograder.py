d53bad613e78f6fc9aaa20a2b4846db89df8abf2
Chris Lai <chrisclai02@gmail.com>
2024-03-20 22:54:08 -0400

    Fixed bug where 00 at start of plaintext breaks things

--- autograder.py
+++ autograder.py
@@ -40,6 +40,13 @@ lab_id = [
     "labf",
 ]
 
+# Pads the string to 32 characters long
+def pad(input):
+    text = input
+    while (len(text) < 32):
+        text = "00" + text
+    return text
+
 def main():
     key = int(KEY, 16)
     aes = AES(key)
@@ -85,14 +92,16 @@ def main():
     message1 = f"{lab}|{score}|{maxscore}|"
     encoded_message = int(message1.encode('utf-8').hex(), 16)
     encrypted1 = hex(aes.encrypt(encoded_message))
+    encrypted1 = pad(encrypted1[2:])
     print(f"data: [{message1}], encoded_message: [{encoded_message}], encrypted: [{encrypted1}]")
     
     message2 = f"{username}"
     encoded_message = int(message2.encode('utf-8').hex(), 16)
     encrypted2 = hex(aes.encrypt(encoded_message))
+    encrypted2 = pad(encrypted2[2:])
     print(f"user: {message2}, encoded_message: {encoded_message}, encrypted: [{encrypted2}]")
 
-    encrypted = encrypted1[2:] + encrypted2[2:]
+    encrypted = encrypted1 + encrypted2
 
     print("\n==========================")
     print("==========================\n")
@@ -101,6 +110,28 @@ def main():
     print("=================================================================")
     print(f"{encrypted}")
     print("=================================================================")
+
+    message = ""
+    for i in range(0, len(encrypted), 32):
+        try:
+            cipher_chunk = int(encrypted[i:(i+32)], 16)
+        except ValueError:
+            print("Error: Chunk size incorrect")
+
+        message_chunk = aes.decrypt(cipher_chunk)
+        message_chunk = bytearray.fromhex("{:x}".format(message_chunk)) # OpenEdx does not support to_bytes
+        message += "".join([chr(x) for x in message_chunk])
+
+    print(f"Decrypted: {message}")
+
+    message = message.split("|")
+    if 4 > len(message):
+        return print("Error: Struct size incorrect")
+
+    lab = message[0]
+    score = float(message[1])
+    maxPoints = float(message[2])
+    username = ("".join(message[3:])).rstrip('\0')
 
     print("Thank you for using the RACECAR Neo Autograder. Goodbye!\n")
 
